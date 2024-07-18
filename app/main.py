e9652c5ded19277d34f9fcab9680ee9a13ac04c0
raulsharma21 <114191720+raulsharma21@users.noreply.github.com>
2024-07-18 10:48:36 -0700

    State variable example

--- app/main.py
+++ app/main.py
@@ -2,3 +2,17 @@ import streamlit as st
 
 st.header("GPI Streamlit Demo")
 st.write("App is hosted on Azure") 
+
+if 'flag' not in st.session_state:
+    st.session_state['flag'] = False
+
+def set_flag():
+    st.session_state['flag'] = not st.session_state['flag']
+
+st.button("Click me", on_click=set_flag)
+
+st.write(f"Flag state: {st.session_state['flag']}")
+
+if st.session_state['flag']:
+    st.error("### Popup Message")
+    st.code("This is a simulated popup message displayed when the button is pressed.")
